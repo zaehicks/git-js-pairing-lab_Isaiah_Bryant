1fb1c53ec136bb9520f3238230bfe6ec3a2dc8df
bryantw12 <b.ashton522@gmail.com>
2023-10-04 15:12:18 -0400

    added #4

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Code your solutions in this file
+// #1
 const fiveToOneHundred = () => {
     for (let i = 5; i < 101; i++) {
         console.log(i)
@@ -6,10 +7,10 @@ const fiveToOneHundred = () => {
     
 }
 //fiveToOneHundred();
-//test
 
 
 
+// #2
 const multiplesOfThree = () =>{ 
     for (let i = 1; i <= 100; i++){
       if (i % 3 === 0) {
@@ -17,14 +18,25 @@ const multiplesOfThree = () =>{
       }
     }
   };
- // multiplesOfThree()
+  
 
-  const untilNum = (num1) =>{
+  // #3 
+  const multiplesOfThreeOrFive = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+          console.log(i);
+        }
+      }
+    }
+    
+multiplesOfThreeOrFive();
+
+ // //multiplesOfThree()
+
+ const untilNum = (num1) =>{
     for( let i= 1; i <= num1; i++)
     console.log(i)
   }
 
   untilNum(10)
-  
-
   
